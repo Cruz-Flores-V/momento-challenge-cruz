8b33a5f30af61cbb9fc3468f9eac409a8c18754b
Cruz Flores <cruz.tag@gmail.com>
2026-03-03 18:43:26 -0600

    Refactor main function to filter issuance and claims by agent and add date range parameters for issuance and claims retrieval

--- src/logic/operations.ts
+++ src/logic/operations.ts
@@ -1,32 +1,105 @@
-import { Agent, Operation } from './types'
-import { readFromCSV } from './utils'
+import { Agent, Operation } from './types';
+import { readFromCSV } from './utils';
 
-export async function getIssuance () {
-  const operations = await readFromCSV('../resources/issuance.csv')
-  return operations
-}
+export type DateFilter = {
+  startDate: Date;
+  endDate: Date;
+};
 
-export async function getClaims () {
-  const operations = await readFromCSV('../resources/claims.csv')
-  return operations
-}
-
-export function calculateIssuance (agent: Agent, operations: Operation[]) {
-  let issuance = 0
-  for (const op of operations) {
-    if (op.agent === agent.id) issuance += Number(op.amount)
+function filterByDate(
+  operations: Operation[],
+  dateFilter?: DateFilter,
+): Operation[] {
+  const { endDate, startDate } = dateFilter ?? {};
+  const mustFilter = startDate && endDate;
+  if (!mustFilter) {
+    return operations;
   }
-  return issuance
+
+  return operations.filter((operation) => {
+    const operationTimestamp = new Date(operation.date).getTime();
+    return (
+      operationTimestamp >= startDate.getTime() &&
+      operationTimestamp <= endDate.getTime()
+    );
+  });
 }
 
-export function calculateClaims (agent: Agent, operations: Operation[]) {
-  let claims = 0
+enum OperationEnum {
+  Recovery = 'recovery',
+  Reserve = 'reserve',
+  Adjust = 'adjust',
+  Deductible = 'deductible',
+}
+
+function filterByOperation(
+  operations: Operation[],
+  operationType: OperationEnum,
+): Operation[] {
+  return operations.filter(({ operation }) => operation === operationType);
+}
+
+function sumOperationsAmount(operations: Operation[]) {
+  return operations.reduce((acc, curr) => acc + Number(curr.amount), 0);
+}
+
+export async function getIssuance(
+  dateFilter?: DateFilter,
+): Promise<Operation[]> {
+  const operations: Operation[] = await readFromCSV(
+    '../resources/issuance.csv',
+  );
+
+  return filterByDate(operations, dateFilter);
+}
+
+export async function getClaims(dateFilter?: DateFilter): Promise<Operation[]> {
+  const operations: Operation[] = await readFromCSV('../resources/claims.csv');
+
+  return filterByDate(operations, dateFilter);
+}
+
+export function calculateIssuance(agent: Agent, operations: Operation[]) {
+  let issuance = 0;
   for (const op of operations) {
-    if (op.agent === agent.id) claims += Number(op.amount)
+    if (op.agent === agent.id) {
+      issuance += Number(op.amount);
+    }
   }
-  return claims
+
+  return Number(issuance.toFixed(2));
 }
 
-export function calculateBonus (agent: Agent) {
-  return agent.issuance / agent.claims
+export function calculateClaims(agent: Agent, operations: Operation[]) {
+  const reserveOperations = filterByOperation(
+    operations,
+    OperationEnum.Reserve,
+  );
+  const adjustOperations = filterByOperation(operations, OperationEnum.Adjust);
+  const deductibleOperations = filterByOperation(
+    operations,
+    OperationEnum.Deductible,
+  );
+  const recoveryOperations = filterByOperation(
+    operations,
+    OperationEnum.Recovery,
+  );
+  const reserveSum = sumOperationsAmount(reserveOperations);
+  const adjustSum = sumOperationsAmount(adjustOperations);
+  const deductibleSum = sumOperationsAmount(deductibleOperations);
+  const recoverySum = sumOperationsAmount(recoveryOperations);
+
+  return Number(
+    (reserveSum + adjustSum - deductibleSum - recoverySum).toFixed(2),
+  );
+}
+
+export function calculateBonus(agent: Agent) {
+  return agent.issuance > 0
+    ? Number((agent.issuance / agent.claims).toFixed(2))
+    : 0;
+  // calcular la siniestralidad
+  // detectar donde el intervalo donde cae, lo haria con un switch
+  // dependiendo del porcentaje obtenido obtenemos la cantidad del bono aplicando
+  // la formula Bono = (ME * PB) / 100 sobre el porcentaje obtenido
 }
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,32 @@
-import { getAgents, showList } from './logic/agents'
-import { getIssuance, getClaims, calculateIssuance, calculateClaims, calculateBonus } from './logic/operations'
+import { getAgents, showList } from './logic/agents';
+import {
+  getIssuance,
+  getClaims,
+  calculateIssuance,
+  calculateClaims,
+  calculateBonus,
+} from './logic/operations';
 
-async function main () {
-  const agents = await getAgents()
-  const issuance = await getIssuance()
-  const claims = await getClaims()
+async function main() {
+  const startDate = new Date('2025-01-01');
+  const endDate = new Date('2025-06-30');
+  const agents = await getAgents();
+  const issuance = await getIssuance({ startDate, endDate });
+  const claims = await getClaims({ startDate, endDate });
 
   for (const agent of agents) {
-    agent.issuance = calculateIssuance(agent, issuance)
-    agent.claims = calculateClaims(agent, claims)
-    agent.bonus = calculateBonus(agent)
+    const agentIssuances = issuance.filter(
+      (operation) => operation.agent === agent.id,
+    );
+    const agentClaims = claims.filter(
+      (operation) => operation.agent === agent.id,
+    );
+    agent.issuance = calculateIssuance(agent, agentIssuances);
+    agent.claims = calculateClaims(agent, agentClaims);
+    agent.bonus = calculateBonus(agent);
   }
 
-  showList(agents)
+  showList(agents);
 }
 
-main()
+main();
